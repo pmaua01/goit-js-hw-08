0880c956a930e3aafde9fafe6f26ff2f2b30e95a
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-08-19 21:10:04 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const onPlay = function (date) {
+  console.log(date);
+  const save = JSON.stringify(date);
+  localStorage.setItem('videoplayer-current-time', save);
+};
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 500));
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+console.log(currentTime);
+
+player
+  .setCurrentTime(currentTime.seconds)
+  .then(function (seconds) {
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name=email]'),
+  text: document.querySelector('[name=message]'),
+};
+
+const TEXT_INPUT = 'feedback-form-state';
+const formInput = {};
+
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+refs.form.addEventListener('submit', onSubmit);
+showSavedInput();
+
+function onInput(e) {
+  //   console.log(e.target.name);
+  formInput[e.target.name] = e.target.value;
+  //   console.log(formInput);
+  const saveText = JSON.stringify(formInput);
+  //   console.log(saveText);
+  localStorage.setItem(TEXT_INPUT, saveText);
+}
+
+function onSubmit(e) {
+  const save = localStorage.getItem(TEXT_INPUT);
+  e.preventDefault();
+  console.log(JSON.parse(save));
+  localStorage.removeItem(TEXT_INPUT);
+  e.target.reset();
+}
+
+function showSavedInput() {
+  const save = localStorage.getItem(TEXT_INPUT);
+
+  if (save) {
+    const textInsert = JSON.parse(save);
+    console.log(textInsert);
+    textInsert.message
+      ? (refs.text.value = textInsert.message)
+      : (refs.text.value = '');
+
+    textInsert.email
+      ? (refs.email.value = textInsert.email)
+      : (refs.email.value = '');
+
+    // console.log(formInput);
+  }
+}
